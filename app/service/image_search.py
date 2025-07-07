709adf7a1169665c2dfab6f47ee6096d73ac0f98
Huaxing.Wang <whx@tju.edu.cn>
2025-07-07 23:24:27 +0800

    Update search logic.

--- app/service/image_search.py
+++ app/service/image_search.py
@@ -9,7 +9,7 @@ executor = concurrent.futures.ThreadPoolExecutor(max_workers=5)
 
 KB_ID = config["image_id"]
 
-async def image_search(query: str, image_num) -> List[(str, str)]:
+async def image_search(query: str, image_num) -> List[str]:
     results = await retrieve_needle(query, index_id=KB_ID, top_k=image_num)
     all_results = []
     for result in results:
@@ -19,10 +19,10 @@ async def image_search(query: str, image_num) -> List[(str, str)]:
         extension = split_names[-1]
         file_name = title + '.' + extension
         link = f"{config['oss_link']}{file_name}"
-        all_results.append((result['text'], link))
+        all_results.append(f"图片关键字：{result['text']}, 图片链接：{link}")
     return all_results
 
-async def images_search(queries: List[str], image_num: int) -> Dict[str, List[(str, str)]]:
+async def images_search(queries: List[str], image_num: int) -> Dict[str, List[str]]:
     tasks = [image_search(query, image_num) for query in queries]
     results = await asyncio.gather(*tasks)
 
